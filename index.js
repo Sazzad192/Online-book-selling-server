5fdab9666b37584b5007c42f8b5b4b1285be3a0d
Sazzad192 <sazadulbg130@gmail.com>
2022-12-20 02:50:36 +0600

    decorate the database as much as possible

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b4qhmp5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const categoriesCollection = client.db("online-book-selling").collection("categories");
+        const productCollection = client.db("online-book-selling").collection("products");
+        const users = client.db("online-book-selling").collection("users");
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.log())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
